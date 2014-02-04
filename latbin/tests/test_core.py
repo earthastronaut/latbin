1904583c4d1408bcddabe838ece256e719fd91ec
astrodsg_laptop <gregersen.dylan@gmail.com>
2014-02-03 19:05:26 -0700

    created test directory and framework

---
+++ latbin/tests/test_core.py
@@ -0,0 +1,22 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+# Standard Library
+import unittest
+
+# 3rd Party
+
+# Internal
+import latbin
+
+# ########################################################################### #
+
+class TestCore (unittest.TestCase):
+    
+    def __setUp__ (self):
+        super(TestCore, self).__setUp__()
+
+
+# ########################################################################### #
+
+if __name__ == "__main__":
+    unittest.main()
