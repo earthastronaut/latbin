14f3ebdd1c00d6202548c0aeb2e3da22da7c90c1
astrodsg_laptop <gregersen.dylan@gmail.com>
2014-02-07 16:32:08 -0700

    added operation method to the points information and modified operators (e.g. +,-,/,etc) to use this method

--- latbin/point_sets.py
+++ latbin/point_sets.py
@@ -22,85 +22,126 @@ class PointInformation (dict):
     
     def __init__ (self,*args,**kwargs):
         super(PointInformation,self).__init__(*args,**kwargs)
-        
-    def _type_check_other (self,other):
-        if not isinstance(other,dict):
-            raise TypeError("other must be a subclass of dict")
-    
-    def __add__ (self,other):
-        return self.add(other,fill=0)
-            
-#     def __iadd__ (self,other):
-#         self._type_check_other(other)
-#         for key in other:
-#             if self.has_key(key):
-#                 continue
-#             self[key] = other[key]
-#         return self    
-      
+           
     def __setitem__ (self,key,value):
         dict.__setitem__(self,key,value)
-      
-    def _operation (self,other,operator,fill):
-        pass
-     
-    def add (self,other,fill=0):
-        self._type_check_other(other)
-        key_set = set(self.keys())
-        for key in other.keys():
-            key_set.add(key)
-        pi = PointInformation()
-        for key in key_set:
-            first = self.get(key, fill)
-            second = other.get(key, fill)
-            pi[key] = first + second
-        return pi
-    
-    def sub (self,other,fill=0):
-        self._type_check_other(other)
-        key_set = set(self.keys())
-        for key in other.keys():
-            key_set.add(key)
-        pi = PointInformation()
-        for key in key_set:
-            first = self.get(key, fill)
-            second = other.get(key, fill)
-            pi[key] = first - second
-        return pi
-        pass
-    
-    def mul (self,other,fill=1):
-        self._type_check_other(other)
-        key_set = set(self.keys())
-        for key in other.keys():
-            key_set.add(key)
-        pi = PointInformation()
-        for key in key_set:
-            first = self.get(key, fill)
-            second = other.get(key, fill)
-            pi[key] = first * second
-        return pi
-     
-    def div (self,other,fill=np.nan):
-        self._type_check_other(other)
-        key_set = set(self.keys())
-        for key in other.keys():
-            key_set.add(key)
-        pi = PointInformation()
-        for key in key_set:
-            first = self.get(key, fill)
-            second = other.get(key, fill)
-            pi[key] = first / second
-        return pi
-    
+ 
     def copy (self):
         return deepcopy(self)
+     
+    pass 
+    # --------------------------------------------------------------------------- #
+      
+    def operation (self,other,operator,fill=np.nan):
+        """
+        This performs an operation on the information
+        
+        Parameters
+        ----------
+        other : dict, PointInformation, any
+        operator : callable, takes two values
+        fill : any
+        
+        Returns
+        -------
+        point_info : PointInformation
+        
+        
+        """
+        point_info = PointInformation()
+        
+        if not isinstance(other,dict):
+            for key in self.keys():
+                point_info[key] = operator(self[key],other)
+            return point_info
+         
+        key_set = set(self.keys())
+        for key in other.keys():
+            key_set.add(key)
+        for key in key_set:
+            first = self.get(key, fill)
+            second = other.get(key, fill)
+            point_info[key] = operator(first,second)
+            
+        return point_info
+ 
+    def inplace_operation (self,other,operator,fill=np.nan):
+        """
+        This performs an operation on the information
+        
+        Parameters
+        ----------
+        other : dict, PointInformation, any
+        operator : callable, takes two values
+        fill : any
+        
+        Returns
+        -------
+        None - changes the current data in self
+        
+        
+        """
+        if not isinstance(other,dict):
+            for key in self.keys():
+                self[key] = operator(self[key],other)
+        
+        for key,second in other.iteritems():
+            first = self.get(key,fill)
+            self[key] = operator(first,second)            
+                 
+    def __add__ (self,other):
+        # Note: having this as a separate function call is slightly slower
+        # computationally for low numbers (~10), but at high numbers (>100) is 
+        # a small effect. 
+        return self.operation(other, operator=lambda x,y:x+y, fill=0)
+
+    def __iadd__ (self,other):
+        self.inplace_operation(other, operator=lambda x,y:x+y, fill=0)
+        
+    def __sub__ (self,other):
+        return self.operation(other, operator=lambda x,y:x-y, fill=0)    
+
+    def __isub__ (self,other):
+        return self.inplace_operation(other, operator=lambda x,y:x-y, fill=0)    
+
+    def __mul__ (self,other):
+        return self.operation(other, operator=lambda x,y:x*y, fill=1)    
+
+    def __imul__ (self,other):
+        return self.inplace_operation(other, operator=lambda x,y:x*y, fill=1)    
+
+    def __div__ (self,other):
+        return self.operation(other, operator=lambda x,y:x/y, fill=np.nan)        
+     
+    def __idiv__ (self,other):
+        return self.inplace_operation(other, operator=lambda x,y:x*y, fill=1)    
     
+    def __rshift__ (self,other):
+        return self.inplace_operation(other, operator=lambda x,y:x<<y, fill=0)    
+
+    def __irshift__ (self,other):
+        return self.inplace_operation(other, operator=lambda x,y:x<<y, fill=0)        
+
+    def __neg__ (self):
+        return self.__mul__(-1)
+
+    def add (self,other,fill=0):
+        return self.operation(other, operator=lambda x,y:x+y, fill=fill)
+        
+    def sub (self,other,fill=0):
+        return self.operation(other, operator=lambda x,y:x-y, fill=fill)    
+    
+    def mul (self,other,fill=1):
+        return self.operation(other, operator=lambda x,y:x*y, fill=fill)    
+     
+    def div (self,other,fill=np.nan):
+        return self.operation(other, operator=lambda x,y:x/y, fill=fill)            
+        
 class PointSet (object):
     
     def __init__ (self, point_coordinates):
         """
-        Paramters
+        Parameters
         ---------
         point_coordinates : ndarray, size=(n_points , n_dims) 
         
