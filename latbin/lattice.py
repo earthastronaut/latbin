684347d2eb653ef12f907d7f29522e8531844ccc
Tim Anderton <quidditymaster@gmail.com>
2014-02-13 14:26:45 -0700

    fixed a latbin.histogram bug and implemented DLattice quantization

--- latbin/lattice.py
+++ latbin/lattice.py
@@ -21,9 +21,7 @@ class Lattice (PointSet):
     The basic Lattice class. To have a specific lattice please use the subclass
     
     """
-    def __init__(self, ndim, origin, scale, rotation, packing_radius):
-        
-        
+    def __init__(self, ndim, origin, scale, rotation):
         if ndim <= 0:
             raise ValueError("ndim must be > 0")
         self.ndim = ndim
@@ -42,16 +40,13 @@ class Lattice (PointSet):
             self.scale = np.ones(ndim,dtype=float)*scale
         if len(self.scale) != ndim or self.scale.ndim != 1:
             raise ValueError("scale must be a float or a float vector of length={}".format(ndim))
-
-        if packing_radius != 1.0:
-            self.scale /= packing_radius 
-
+        
         # TODO: finish rotation
         if rotation is None:
             rotation = np.eye(ndim) 
         self.rotation = np.asarray(rotation)
-     
-    def to_data_coords(self, lattice_coords):
+    
+    def lattice_to_data_space(self, lattice_coords):
         """
         transforms from the internal lattice coordinates to the original 
         data coordinates.
@@ -71,7 +66,7 @@ class Lattice (PointSet):
         
         return data_coords        
      
-    def to_lattice_coords (self,data_coords):
+    def data_to_lattice_space (self,data_coords):
         """
         transforms from the internal lattice coordinates to the original 
         data coordinates.
@@ -91,14 +86,21 @@ class Lattice (PointSet):
             lattice_coords = (data_coords-self.origin)/self.scale
         
         return lattice_coords        
-   
+
+    def quantize(self, points):
+        raise Exception("Lattice isn't meant to be used this way see the generate_lattice helper function")
+    
+    def representation_to_centers(self, representations):
+        raise Exception("Lattice isn't meant to be used this way see the generate_lattice helper function")
+
 class ZLattice (Lattice):
     
-    def __init__ (self, ndim, origin=None, scale=None, rotation=None, packing_radius=1.0):
-        Lattice.__init__(self, ndim, origin, scale, rotation, packing_radius)
-
-    def coordinates(self,point_index):
-        return self.to_data_coords(point_index)
+    def __init__ (self, ndim, origin=None, scale=None, rotation=None):
+        Lattice.__init__(self, ndim, origin, scale, rotation)
+        self.minimal_norm = 1.0
+    
+    def representation_to_centers(self, representations):
+        return self.lattice_to_data_space(representations - 0.5)
     
     def quantize (self,points,return_distortion=False):
         """
@@ -111,24 +113,49 @@ class ZLattice (Lattice):
         ------
         
         """
-        return Lattice.quantize(self, points, return_distortion)
+        lspace_pts = self.to_lattice_space(points)
+        return np.array(lspace_pts + 0.5, dtype=int)
 
 class DLattice (Lattice):
 
-    def __init__ (self, ndim, origin=None, scale=None, rotation=None, packing_radius=1.0):
-        Lattice.__init__(self, ndim, origin, scale, rotation, packing_radius)
-       
+    def __init__ (self, ndim, origin=None, scale=None, rotation=None):
+        Lattice.__init__(self, ndim, origin, scale, rotation)
+        self.minimal_norm = np.sqrt(2)
+    
+    def quantize(self, points):
+        lspace_pts = self.data_to_lattice_space(points)
+        rounded_pts = np.around(lspace_pts)
+        csum = np.sum(rounded_pts, axis=-1)
+        cdiff = lspace_pts - rounded_pts
+        abs_cdiff = np.abs(cdiff)
+        delta_max_idxs = np.argmax(np.abs(cdiff), axis=-1)
+        quantized_repr = np.array(rounded_pts, dtype=int)
+        for i in range(len(quantized_repr)):
+            if csum[i] % 2 == 1:
+                max_idx = delta_max_idxs[i]
+                if cdiff[i, max_idx] < 0:
+                    #we rounded up round down instead
+                    quantized_repr[i, max_idx] -= 1
+                else:
+                    quantized_repr[i, max_idx] += 1
+        return quantized_repr
+                
+    
+    def representation_to_centers(self, representations):
+        return self.lattice_to_data_space(representations)
+
 class ALattice (Lattice):
 
-    def __init__ (self, ndim, origin=None, scale=None, rotation=None, packing_radius=1.0):
-        Lattice.__init__(self, ndim, origin, scale, rotation, packing_radius)
+    def __init__ (self, ndim, origin=None, scale=None, rotation=None):
+        Lattice.__init__(self, ndim, origin, scale, rotation)
 
 class ELattice (Lattice):
 
-    def __init__ (self, ndim, origin=None, scale=None, rotation=None, packing_radius=1.0):
-        Lattice.__init__(self, ndim, origin, scale, rotation, packing_radius)
-       
+    def __init__ (self, ndim, origin=None, scale=None, rotation=None):
+        Lattice.__init__(self, ndim, origin, scale, rotation)
+
 families = {'z':ZLattice,
+            'd':DLattice
             }
 # TODO: add class to families when known
 
@@ -142,10 +169,13 @@ def histogram(points, bins):
     keys = bins.quantize(points)
     pi = PointInformation()
     for i, key in enumerate(keys):
-        pi[key] = points[i]
+        pi_num = pi.get(key)
+        if pi_num == None:
+            pi[key] = 1
+        else:
+            pi[key] += 1
     return pi
 
- 
 # ########################################################################### #
  
 def generate_lattice (ndim, origin=None, scale=None, family="z", packing_radius=1.0):
