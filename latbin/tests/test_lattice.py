cf83a47cc3afda223ea1e442b96fc8dbea6842e1
astrodsg_laptop <gregersen.dylan@gmail.com>
2014-03-03 14:38:41 -0700

    Broke up the point_information tests into it's own file Fixed tests to reflect lattice being immutable

--- latbin/tests/test_lattice.py
+++ latbin/tests/test_lattice.py
@@ -7,67 +7,9 @@ import unittest
 import numpy as np
 
 # Internal
-from latbin import PointInformation
 from latbin import ZLattice, DLattice, ALattice, Lattice, generate_lattice
 
 # ########################################################################### #
-
-class TestPointInformation (unittest.TestCase):
-    
-    def setUp(self):
-        
-    
-        self.pi1 = PointInformation(a=1,b=3,c='hello',e=[1,2,3,4])
-        self.pi2 = PointInformation(    b=2,c=' world',e=[5,6])
-        
-        self.pi3 = PointInformation(a=3.2,b=2.3,c=1.2)
-        self.pi4 = PointInformation(      b=5,  c=0, d=-2.3)
-        
-        unittest.TestCase.setUp(self)   
-
-    def test_add (self):
-        
-        PI = PointInformation
-        
-        # these pairs are the (test,answer)
-        rpairs = [((self.pi3, self.pi4), PI(a=3.2,b=7.3,c=1.2,d=-2.3)),
-                  ((self.pi1, self.pi2), PI(a=1,b=5,c='hello world',e=[1,2,3,4,5,6])),
-                  #((self.pi3, self.pi4), PI(a=3.2,b=7.3,c=1.2,d=-2.3)),
-                  #((self.pi3, self.pi4), PI(a=3.2,b=7.3,c=1.2,d=-2.3)),
-                  ]
-        
-        for i,(operands, true_result) in enumerate(rpairs):
-            op1, op2 = operands
-            add_result = op1.add(op2,fill=0)
-            
-            msg = " .add failed for example {}".format(i)
-            self.assertEqual(add_result, true_result , msg)
-        
-            dunder_add_result = op1 + op2
-            msg = " __add__  failed for example {}".format(i)
-            self.assertEqual(dunder_add_result, true_result , msg)
-        
-    def test_operation(self):
-        PI = PointInformation
-        rpairs = [((self.pi3, self.pi4), PI(a=3.2,b=7.3,c=1.2,d=-2.3)),
-                  ((self.pi1, self.pi2), PI(a=1,b=5,c='hello world',e=[1,2,3,4,5,6])),
-                  ]
-        
-        for i,(operands, true_result) in enumerate(rpairs):
-            op1, op2 = operands
-            add_result = op1.operation(op2,lambda x,y: x+y,fill=0)
-            msg = " .operation failed for example {}".format(i)
-            self.assertEqual(add_result, true_result , msg)
-
-
-            op1.inplace_operation(op2,lambda x,y: x+y,fill=0)
-            msg = " .inplace_operation failed for example {}".format(i)
-            self.assertEqual(op1, true_result , msg)
-    
-            try:
-                op1.inplace_operation(op2,lambda x: x,fill=0)
-            except Exception as e:
-                self.assert_(isinstance(e,TypeError), "Takes operators that have args=1")    
 
 class TestLattice (unittest.TestCase):
     
@@ -160,19 +102,15 @@ class TestLattice (unittest.TestCase):
                 rpoints = self.rpoints[dim]
                 quant = lat.quantize(self.rpoints[dim])
                 dspace_centers = lat.representation_to_centers(quant)
-                import matplotlib.pyplot as plt
-                for i in range(len(quant)):
-                    plt.plot([rpoints[i, 0], dspace_centers[i, 0]], [rpoints[i, 1], dspace_centers[i, 1]], alpha=0.5)
+                #import matplotlib.pyplot as plt
+                #for i in range(len(quant)):
+                #    plt.plot([rpoints[i, 0], dspace_centers[i, 0]], [rpoints[i, 1], dspace_centers[i, 1]], alpha=0.5)
                 q_error = dspace_centers - rpoints
                 #plt.hist(q_error[:, 0], 100)
                 #plt.hist(q_error[:, 1], 100)
                 #plt.scatter(q_error[:, 0], q_error[:, 1])
-                plt.show()
-                import pdb; pdb.set_trace()
-                
-        
-        
-    
+                #plt.show()
+                pass
 
 # ########################################################################### #
 
