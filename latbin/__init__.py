140f665aeb539825fbc55e1aefd8de1e50595d55
Tim Anderton <quidditymaster@gmail.com>
2015-08-17 14:41:12 -0600

    included interpolation module in __init__ and updated version number to 0.1.5

--- latbin/__init__.py
+++ latbin/__init__.py
@@ -1,8 +1,8 @@
 # Licensed under a 3-clause BSD style license - see LICENSE
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
-from latbin.point_information import *
-from latbin.lattice import *
-import latbin.matching as matching
-import latbin.scaling as scaling
+from .lattice import *
+from . import matching
+from . import scaling
+from . import interpolation
