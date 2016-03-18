f4892ead906d4ed5db7682622767b56e7085f49b
Tim Anderton <quidditymaster@gmail.com>
2016-03-18 12:24:28 -0600

    updated z2 vs a2 comparison script

--- docs/examples/z2_vs_a2.py
+++ docs/examples/z2_vs_a2.py
@@ -2,6 +2,7 @@
 
 import latbin
 import numpy as np
+import matplotlib.pyplot as plt
 
 # create some fake data with shape (npts,ndim)
 npts,ndim = 60000,2
@@ -13,23 +14,25 @@ z2 = latbin.ZLattice(2)
 # create an A2 lattice (extension to the honeycomb lattice)
 a2 = latbin.ALattice(2)
 
-# histogram the data onto both lattices
-h1 = z2.histogram(data)
-h2 = a2.histogram(data)
+# bin the data onto the lattices
+h1 = z2.bin(data)
+h2 = a2.bin(data)
 
-# get the lattice points in the data space
-centers1 = h1.centers()
-centers2 = h2.centers()
+# get the mean of the data in each bin
+centers1 = h1[[0, 1]].mean().values
+centers2 = h2[[0, 1]].mean().values
+#find the number of points in each bin
+n1 = h1.size().values
+n2 = h2.size().values
 
 # show the result
-import matplotlib.pylab as plt
 fig,ax = plt.subplots(1,2,sharex=True,sharey=True)
 
 ax[0].set_title("Square binning (Z2 Lattice)")
-ax[0].scatter(centers1[:,0],centers1[:,1],c=h1.values(), s=70)
+ax[0].scatter(centers1[:,0],centers1[:,1],c=n1, s=70)
 
 ax[1].set_title("Honeycomb binning (A2 Lattice)")
-ax[1].scatter(centers2[:,0],centers2[:,1],c=h2.values(), s=70)
+ax[1].scatter(centers2[:,0],centers2[:,1],c=n2, s=70)
 
 plt.show()
 
