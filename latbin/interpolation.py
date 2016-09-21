a035c03c1184201b89509a6a185354d26762a8cb
Tim Anderton <quidditymaster@gmail.com>
2016-09-21 15:28:23 -0600

    added option for interpolator to estimate variance

--- latbin/interpolation.py
+++ latbin/interpolation.py
@@ -11,25 +11,41 @@ class KernelWeightedMatchingInterpolator(object):
     
     def __init__(self, x, y, x_scale, weighting_kernel=None, match_tolerance=6.0):
         if weighting_kernel is None:
-            weighting_kernel = lambda x: np.exp(-0.5*x**2)/(1.0+20.0*x**2)
+            weighting_kernel = lambda x: np.exp(-0.5*x**2)#/(1.0+(x/0.1)**2)
         self.weighting_kernel = weighting_kernel
         self.x_scale = x_scale
         self.m_indexer = MatchingIndexer(x/self.x_scale, tolerance=match_tolerance)
         self.y = y
     
-    
-    def __call__(self, x_interp):
+    def __call__(self, x_interp, estimate_variance=False):
         x_interp = x_interp/self.x_scale
         dmat = self.m_indexer.distance_matrix(x_interp)
-        import matplotlib.pyplot as plt
-        import pdb; pdb.set_trace()
         dmat.data = self.weighting_kernel(dmat.data)
         
         weighted_data = dmat*self.y
         weight_sums = dmat*np.ones(len(self.y))
         
         if len(weighted_data.shape) == 1:
-            return weighted_data/weight_sums
+            y_interp = weighted_data/weight_sums
         else:
-            return weighted_data/weight_sums.reshape((-1, 1))
-
+            y_interp = weighted_data/weight_sums.reshape((-1, 1))
+        
+        if not estimate_variance:
+            return y_interp
+        else:
+            sq_diff_sums = np.zeros(y_interp.shape)
+            dmat_sort = dmat.tocsc().sorted_indices()
+            indices = dmat_sort.indices
+            indptr = dmat_sort.indptr
+            for col_idx in range(len(indptr)-1):
+                lbi = indptr[col_idx]
+                ubi = indptr[col_idx+1]
+                row_indices = indices[lbi:ubi]
+                data_weight = dmat_sort.data[lbi:ubi]
+                if len(row_indices) > 0:
+                    for row_index, dweight in zip(row_indices, data_weight):
+                        delta_y_sq = (self.y[col_idx] - y_interp[row_index])**2
+                        sq_diff_sums[row_index] += delta_y_sq*dweight
+            
+            estimated_var = sq_diff_sums/weight_sums
+            return y_interp, estimated_var
